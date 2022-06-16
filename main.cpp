1d8043f5e7824e084256612964ea0740f64d40f9
kuone <kuone@example.com>
2022-06-16 15:54:04 +0900

    Iteratorパターンで走査する処理のサンプルを実装

--- main.cpp
+++ main.cpp
@@ -1,4 +1,34 @@
+#include <memory>
+#include <iostream>
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template<class T>
+struct Iterator {
+	virtual void Next()          = 0 ;
+	virtual T    Item()    const = 0 ;
+	virtual bool HasNext() const = 0 ;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+std::unique_ptr<Iterator<int>> Sequence( const int limit ) {
+	struct Impl :Iterator<int> {
+		void Next() override { m_val++ ; }
+		int  Item() const override { return m_val ; }
+		bool HasNext() const override { return m_val < m_limit ; }
+
+		Impl( const int limit ) :m_limit{ limit } {}
+		int m_val = 0 ;
+		int m_limit{} ;
+	};
+
+	return std::make_unique<Impl>( limit );
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
+	for ( auto it = Sequence( 10 ) ; it->HasNext() ; it->Next() ) {
+		const auto item = it->Item() ;
 
+		std::cout << item << std::endl;
+	}
 }
